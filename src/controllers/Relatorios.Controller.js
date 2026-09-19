3e1d691655f1bb080472ad996ddca3799d3d9b18
MatheusXz <49512638+MatheusXz@users.noreply.github.com>
2026-09-19 13:32:12 -0300

    style: agrupa relatorios de marmitas especiais

--- src/controllers/Relatorios.Controller.js
+++ src/controllers/Relatorios.Controller.js
@@ -5,7 +5,23 @@ export const listarCatalogoRelatorios = async (req, res, next) => {
     try {
         const relatorios = await connection('catalogo_relatorios')
             .where('ativo', true)
-            .orderBy('id', 'asc')
+            .orderByRaw(`
+                CASE funcao_db
+                    WHEN 'fn_rel_faturamento_periodo' THEN 10
+                    WHEN 'fn_rel_historico_vendas' THEN 20
+                    WHEN 'fn_rel_saida_alimentos' THEN 30
+                    WHEN 'fn_rel_vendas_tamanho' THEN 40
+                    WHEN 'fn_rel_vendas_marmitas_especiais' THEN 41
+                    WHEN 'fn_rel_clientes_frequentes' THEN 50
+                    WHEN 'fn_rel_listagem_alimentos' THEN 60
+                    WHEN 'fn_rel_listagem_categorias' THEN 70
+                    WHEN 'fn_rel_listagem_tamanhos' THEN 80
+                    WHEN 'fn_rel_listagem_marmitas_especiais' THEN 81
+                    WHEN 'fn_rel_listagem_pagamentos' THEN 90
+                    ELSE 999
+                END ASC,
+                id ASC
+            `)
             .select([
                 'id',
                 'nome',
